cd42d7d1c63681b197a1ce55682946b7652ebfea
Jhonathan Queiroz <jhonathanqueiroz@MacBook-Air-de-Gabuldev.local>
2025-03-28 15:59:46 -0300

    fix remove import

--- android/src/main/java/dev/gabul/pagseguro_smart_flutter/PagseguroSmartFlutterPlugin.java
+++ android/src/main/java/dev/gabul/pagseguro_smart_flutter/PagseguroSmartFlutterPlugin.java
@@ -19,7 +19,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+// import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 public class PagseguroSmartFlutterPlugin
   implements FlutterPlugin, MethodCallHandler {
